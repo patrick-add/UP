75ea137bbae14907f9bb511677bbbfbd8e406cae
Patrick Andrade <112036293+patrick-add@users.noreply.github.com>
2023-01-25 23:55:59 -0300

    Atualizando

--- 17 - Primeiro Servidor/01/src/index.js
+++ 17 - Primeiro Servidor/01/src/index.js
@@ -3,14 +3,18 @@ const app = express()
 
 const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
 
-let contador = 0
-app.get('/', (req, res) => {
-    res.send(`É a vez de ${jogadores[contador]} jogar!`)
-    if (contador === jogadores.length - 1) {
-        contador = 0
-    } else {
-        contador++
+let jogadorDaVez = 0;
+
+app.get("/", (req, res) => {
+    const nomeJogador = jogadores[jogadorDaVez];
+
+    jogadorDaVez++;
+
+    if (jogadorDaVez >= jogadores.length) {
+        jogadorDaVez = 0;
     }
-})
+
+    return res.send(`É a vez de ${nomeJogador} jogar!`);
+});
 
 app.listen(3000)
